169eec1625c9013a83e6ed18f8eb3446171c9e3d
DroidekaMuSo <diego.munoz_solis@hotmail.com>
2023-02-14 14:42:28 -0600

    Realizacion de proyectos de clase"

---
+++ clase5/express_handlebars_p2/src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+
+const viewsRoute = require("./routes/views.routes");
+const usersRoute = require("./routes/user.routes");
+
+const PORT = 5_000;
+const app = express();
+const BASE_PREFIX = "api";
+
+//Handlers
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(`${__dirname}/views`));
+app.set("view engine", "handlebars");
+
+app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", viewsRoute);
+app.use(`/${BASE_PREFIX}/users`, usersRoute);
+
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
---
+++ clase5/express_handlebars_p2/src/utils.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(`${__dirname}/public/uploads/`));
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const uploader = multer({
+  storage,
+  onError: (err, next) => {
+    console.log(err);
+    next();
+  },
+});
+
+module.exports = {
+  uploader,
+};
