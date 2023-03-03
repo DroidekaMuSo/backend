6b2c0e6fd1abe2573068e52d68b5900395c5f25d
DroidekaMuSo <diego.munoz_solis@hotmail.com>
2023-03-02 19:50:23 -0600

    Actualizacion de proyectos

--- clase7/mongo-crud-p2/src/app.js
+++ clase7/mongo-crud-p2/src/app.js
@@ -1,36 +1,37 @@
-//dependecies
-const mongoose = require("mongoose"),
-  express = require("express"),
+const express = require("express"),
+  mongoose = require("mongoose"),
   displayRoutes = require("express-routemap");
 
-//Routes
-const studentsRoutes = require("./routes/students.routes"),
-  petsRoutes = require("./routes/pets.routes");
-
-//Server settings
 const PORT = 5000,
   DB_HOST = "localhost",
-  DB_PORT = 27017,
+  DB_PORT = "27017",
   DB_NAME = "baseCRUD";
 
+console.log("🚀 ~ file: app.js:6 ~ PORT:", PORT);
+console.log("🚀 ~ file: app.js:8 ~ DB_HOST:", DB_HOST);
+console.log("🚀 ~ file: app.js:10 ~ DB_PORT:", DB_PORT);
+console.log("🚀 ~ file: app.js:12 ~ DB_NAME:", DB_NAME);
+console.log(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const connection = mongoose
-  .connect(`mongodb://${DB_HOST};${DB_PORT}/${DB_NAME}`)
+  .connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
   .then((conn) => {
-    console.log("Connected");
+    console.log("🚀 ~ file: app.js:18 ~ conn:", conn);
+    return;
   })
   .catch((err) => {
-    console.log(err);
+    console.log("🚀 ~ file: app.js:21 ~ err:", err);
   });
 
-app.use("/api/pets", petsRoutes);
-app.use("/api/students", studentsRoutes);
+app.use("/", (req, res) => {
+  res.json({ message: "OK" });
+});
 
 app.listen(PORT, () => {
   displayRoutes(app);
-  console.log(`Running on port: ${PORT}`);
+  console.log(`Running on port ${PORT}`);
 });
